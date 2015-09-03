be962a215aea8c30022c872ad939da1df40de968
apierce1992 <apierce1992@gmail.com>
2015-09-03 15:34:57 -0400

    Close to solution but no cigar

--- word_frequency.py
+++ word_frequency.py
@@ -13,11 +13,15 @@ def word_frequency(string):
             freq[word] = 1
         else: freq[word] += 1
     freq = sorted(freq.items(), key = lambda c: c[1], reverse = True)
-    return freq[0:20]
+    freq = freq[0:20]
+    freq = dict(freq)
+    return freq
 
 if __name__ == '__main__':
     main("sample.txt")
 
-#Problem: Program is return a LIST of tuples,
-#not a dictionary with values, tests are looking for dictionary
-# Test is looking for number output
+#Returns a dict of top 20 used words, NOT in order,
+#b/c dicts are unordered
+# test fails if example is not in top 20
+#Options: Figure out how to order dictionary & preserve full contents
+#Figure out how to return numbers in word_frequency and list contents through main
