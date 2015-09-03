6c638b5c23f4523c211946562eab5ec704f62fdd
apierce1992 <apierce1992@gmail.com>
2015-09-03 12:44:14 -0400

    Added version that creates word count

--- word_frequency.py
+++ word_frequency.py
@@ -1,2 +1,22 @@
-def word_frequency(text):
-    return {"hello": 1}
+import re
+
+def file(link):
+    file = open(link)
+    sample = file.read()
+    return sample
+
+def word_frequency(string):
+    words = re.sub(r'[^A-Za-z0-9 ]', ' ', string).lower().split()
+    print(words)
+    worddict = {}
+    for word in words:
+        if word not in worddict:
+            worddict[word] = int(1)
+        else: worddict[word] += 1
+    return print(worddict)
+
+
+
+
+
+word_frequency(file("sample.txt"))
