6a36f3db58c7e0d910442c15a2734d429284cee1
apierce1992 <apierce1992@gmail.com>
2015-09-02 21:49:12 -0400

    Added sample files, etc

---
+++ frequency-filer.py
@@ -0,0 +1,8 @@
+#open sample.txt
+#strip/split/lower = new_list
+#if word doesnt exist new entry in dict
+#else word value + 1
+#sort word value for 20
+import urllib2
+sample = urlib2.urlopen(https://github.com/tiyd-python-2015-08/assigments/blob/master/04--Frequency-Filer/sample.txt)
+sample.read()
