faf1f9ca67afaeceb31fe82941b23a7e077a388c
apierce1992 <apierce1992@gmail.com>
2015-09-03 14:34:08 -0400

    Returns list of tuples

--- word_frequency.py
+++ word_frequency.py
@@ -10,10 +10,14 @@ def word_frequency(string):
     freq = {}
     for word in words:
         if word not in freq:
-            freq[word] = int(1)
+            freq[word] = 1
         else: freq[word] += 1
-    freq = sorted(freq, key = freq.get, reverse = True)
+    freq = sorted(freq.items(), key = lambda c: c[1], reverse = True)
     return freq[0:20]
 
 if __name__ == '__main__':
     main("sample.txt")
+
+#Problem: Program is return a LIST of tuples,
+#not a dictionary with values, tests are looking for dictionary
+# Test is looking for number output
