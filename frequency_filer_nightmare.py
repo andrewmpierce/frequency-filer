b83ab2974a8920b55da88023e6bc99a20be26e56
apierce1992 <apierce1992@gmail.com>
2015-09-04 00:20:16 -0400

    Finished nightmare mode

---
+++ frequency_filer_nightmare.py
@@ -0,0 +1,37 @@
+import re
+import sys
+
+
+def main():
+    link = sys.argv[1]
+    file = open(link)
+    sample = file.read()
+    return loop(word_frequency(sample))
+
+def loop(dictionary):
+    counter = 0
+    sorted_list = sorted(dictionary.items(), key = lambda c: c[1], reverse = True)
+    for x in range(20):
+        if sorted_list[counter][1] >= 50:
+            print(sorted_list[counter][0] + ' ' + ('#' * 50))
+        else:
+             print(sorted_list[counter][0] + ' ' + ('#' * sorted_list[counter][1]))
+        counter += 1
+
+
+def word_frequency(string):
+    ignored_words = ['a', 'the', 'you', 'he', 'she', 'it', 'for']
+    words = re.sub(r'[^A-Za-z0-9 ]', ' ', string).lower().split()
+    freq = {}
+    for word in words:
+        if word not in ignored_words:
+            if word not in freq:
+                freq[word] = 1
+            else: freq[word] += 1
+    for word in ignored_words:
+        if word in freq:
+            del freq[word]
+    return freq
+
+if __name__ == '__main__':
+    main()
