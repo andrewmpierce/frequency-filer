538a503aa01df71d989a43298d9e34dda673d35e
apierce1992 <apierce1992@gmail.com>
2015-09-03 21:17:05 -0400

    Finished normal mode

--- word_frequency.py
+++ word_frequency.py
@@ -3,7 +3,15 @@ import re
 def main(link):
     file = open(link)
     sample = file.read()
-    print(word_frequency(sample))
+    return loop(word_frequency(sample))
+
+
+def loop(dictionary):
+    counter = 0
+    sorted_list = sorted(dictionary.items(), key = lambda c: c[1], reverse = True)
+    for x in range(20):
+        print(sorted_list[counter][0] + ' ' + str(sorted_list[counter][1]))
+        counter += 1
 
 def word_frequency(string):
     words = re.sub(r'[^A-Za-z0-9 ]', ' ', string).lower().split()
@@ -12,9 +20,6 @@ def word_frequency(string):
         if word not in freq:
             freq[word] = 1
         else: freq[word] += 1
-    freq = sorted(freq.items(), key = lambda c: c[1], reverse = True)
-    freq = freq[0:20]
-    freq = dict(freq)
     return freq
 
 if __name__ == '__main__':
